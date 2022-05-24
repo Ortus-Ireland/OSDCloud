bb278337b8eea428db3efa91829ce1c60b05fb2e
DaveBrophy <40424676+DaveBrophy@users.noreply.github.com>
2022-05-24 12:27:52 +0100

    Update win10.ps1

--- win10.ps1
+++ win10.ps1
@@ -22,7 +22,7 @@ Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
 
 Import-Module OSD -Force
 
-Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
+#Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Internet Explorer\Main" -Name "DisableFirstRunCustomize" -Value 2
 
 $ScriptFromGitHub = Invoke-Restmethod https://raw.githubusercontent.com/OSDeploy/OSD/9484db58a67f10362e31613d94ac3f15db78fe2a/Private/Disk/Diskpart-Clean.ps1
 Invoke-Expression $ScriptFromGitHub
