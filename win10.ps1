41051e9567e02d5792db3e8a23cd4c16693e6556
DaveBrophy <40424676+DaveBrophy@users.noreply.github.com>
2022-05-24 14:57:52 +0100

    Update win10.ps1

--- win10.ps1
+++ win10.ps1
@@ -28,7 +28,7 @@ Import-Module OSD -RequiredVersion 22.5.10.1 -Force
 Write-Host -ForegroundColor Green "Start OSDCloud"
 
 #Start-OSDCloud -OSLanguage en-gb -OSBuild 21H2 -OSEdition Pro -ZTI -SkipAutopilot 
-Start-OSDCloud -OSLanguage en-gb -OSBuild 21H2 -OSEdition Pro -SkipAutopilot -ImageFileUrl "http://wds/esd/win10pro21H2.esd" -ZTI
+Start-OSDCloud -ImageFileUrl "http://wds/esd/win10pro21H2.esd" -ImageIndex 5 -ZTI
 
 #Restart from WinPE
 
