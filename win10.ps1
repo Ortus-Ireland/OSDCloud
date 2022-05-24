85c1d3561e4365d8686d3ec3358404f3d9f5bdb7
DaveBrophy <40424676+DaveBrophy@users.noreply.github.com>
2022-05-24 12:01:06 +0100

    Update win10.ps1

--- win10.ps1
+++ win10.ps1
@@ -22,6 +22,29 @@ Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
 
 Import-Module OSD -Force
 
+function Diskpart-Clean {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0, Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [string]$DiskNumber
+    )
+    #Virtual Machines have issues using PowerShell for Clear-Disk
+    #$OSDDisk | Clear-Disk -RemoveOEM -RemoveData -Confirm:$true -PassThru -ErrorAction SilentlyContinue | Out-Null
+    
+    Write-Verbose "DISKPART> select disk $DiskNumber"
+    Write-Verbose "DISKPART> clean"
+    Write-Verbose "DISKPART> exit"
+    
+    #Abort if not in WinPE
+    if ($env:SystemDrive -ne "X:") {Return}
+
+$null = @"
+select disk $DiskNumber
+clean
+exit 
+"@ | diskpart.exe
+}
+
 #Start OSDCloud ZTI the RIGHT way
 
 Write-Host -ForegroundColor Green "Start OSDCloud"
