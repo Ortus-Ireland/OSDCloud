1a36e7f8263f386d0183b56b3c0b66d1ea3d4e19
DaveBrophy <40424676+DaveBrophy@users.noreply.github.com>
2023-02-14 12:57:06 +0000

    Update win11.ps1

--- win11.ps1
+++ win11.ps1
@@ -2,6 +2,9 @@
 #$CustomImageFile = "http://wds/esd/win10pro21H2.esd"; $index = 8 # Windows 10 Image
 $CustomImageFile = "http://wds/esd/win11pro21H2drivers.esd"; $Index = 1 # Windows 11 Image
 
+Add-Type -AssemblyName Microsoft.VisualBasic
+$ComputerName = [Microsoft.VisualBasic.Interaction]::InputBox('Computer Name', 'Computer Name', "Enter computer name here")
+Write-Host "Computer will be renamed to $ComputerName once complete"
 Write-Host -ForegroundColor Green "Starting OSDCloud ZTI"
 
 Start-Sleep -Seconds 5
@@ -18,15 +21,59 @@ Set-DisRes 1600
 
 #Make sure I have the latest OSD Content
 
-Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
+#Write-Host -ForegroundColor Green "Updating OSD PowerShell Module"
 
 #Install-Module OSD -RequiredVersion 22.5.10.1 -Force #Get specific version
-Install-Module OSD -Force
+#Install-Module OSD -Force
 
 Write-Host -ForegroundColor Green "Importing OSD PowerShell Module"
 
 #Import-Module OSD -RequiredVersion 22.5.10.1 -Force #Import specific version
 Import-Module OSD
+
+######################
+# Build variables
+######################
+    #=======================================================================
+    # Create Hashtable
+    #=======================================================================
+    $Global:StartOSDCloud = $null
+    $Global:StartOSDCloud = [ordered]@{
+        DriverPackUrl = $null
+        DriverPackOffline = $null
+        DriverPackSource = $null
+        Function = $MyInvocation.MyCommand.Name
+        GetDiskFixed = $null
+        GetFeatureUpdate = $null
+        GetMyDriverPack = $null
+        ImageFileOffline = $null
+        ImageFileName = $null
+        ImageFileSource = $null
+        ImageFileTarget = $null
+        ImageFileUrl = $ImageFileUrl
+        IsOnBattery = Get-OSDGather -Property IsOnBattery
+        Manufacturer = $Manufacturer
+        OSBuild = $OSBuild
+        OSBuildMenu = $null
+        OSBuildNames = $null
+        OSEdition = $OSEdition
+        OSEditionId = $null
+        OSEditionMenu = $null
+        OSEditionNames = $null
+        OSLanguage = $OSLanguage
+        OSLanguageMenu = $null
+        OSLanguageNames = $null
+        OSLicense = $null
+        OSImageIndex = $ImageIndex
+        Product = "none"
+        Screenshot = $null
+        SkipAutopilot = $SkipAutopilot
+        SkipODT = $SkipODT
+        TimeStart = Get-Date
+        ZTI = $ZTI
+    }
+
+
 
 #Start OSDCloud ZTI the RIGHT way
 
@@ -40,6 +87,7 @@ Start-OSDCloud -ImageFileUrl $CustomImageFile -ImageIndex $Index -ZTI -firmware 
 
 Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
 
-Start-Sleep -Seconds 20
+$StartOSDCloud
+#Start-Sleep -Seconds 20
 
-wpeutil reboot
+#wpeutil reboot
