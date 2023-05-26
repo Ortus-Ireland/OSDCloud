67d2901c80a86cea64766cb140f155c1d20d8520
DaveBrophy <40424676+DaveBrophy@users.noreply.github.com>
2023-05-26 09:58:40 +0100

    Update installoffice.ps1

--- installoffice.ps1
+++ installoffice.ps1
@@ -13,21 +13,26 @@ if (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*
     Write-Host "Microsoft 365 Apps is installed"
 }
 else {
+    Write-Host "Installing Microsoft 365 Apps"
     # Install Office using Winget
     winget install --id Microsoft.Office --accept-source-agreements
+    Write-Host "Microsoft 365 Apps is installed"
 
     # Copy Office Shortcuts to Desktop
     copy-item -path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Outlook.lnk" -Destination "c:\users\public\desktop\Outlook.lnk" -Force
     copy-item -path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Word.lnk" -Destination "c:\users\public\desktop\Word.lnk" -Force
     copy-item -path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Excel.lnk" -Destination "c:\users\public\desktop\Excel.lnk" -Force
     copy-item -path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\PowerPoint.lnk" -Destination "c:\users\public\desktop\PowerPoint.lnk" -Force
+    Write-Host "Shortcuts copied to desktop"
 }
 
 # Check if Teams Machine-Wide Installer is installed
 if (Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Where-Object { $_.DisplayName -like "Teams Machine-Wide Installer*" }) {
-    Write-Host "Teams Machine-Wide Installer is installed"
+    Write-Host "Microsoft Teams is installed"
 }
 else {
+    Write-Host "Installing Microsoft Teams"
     # Install Teams Machine-Wide Installer using Winget
     winget install --id Microsoft.Teams --accept-source-agreements
+    Write-Host "Microsoft Teams is installed"
 }
