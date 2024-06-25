1923dd14ccdab0c89d32bc054ca9271a3e437249
nakquada <nakquada@hotmail.com>
2024-06-25 12:38:28 +0100

    Added RAMDisk Modifications

--- imageStaging.ps1
+++ imageStaging.ps1
@@ -3,22 +3,28 @@ Write-Host "Make sure Windows ISO is mounted to D:\ before continuing!" -Foregro
 Write-Host "Press any key to continue or CTRL+C to cancel..."
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 Write-Host ""
+Write-Host "Copying ImageStaging to RAM Disk..." -ForegroundColor white -BackgroundColor blue
+
+## Copy ImageStaging to RAM Disk
+Copy-Item -Path C:\ImageStaging -Destination R:\ImageStaging -recurse -Force
+Write-Host "RAMDisk Initialised..." -ForegroundColor white -BackgroundColor darkgreen
+Write-Host ""
 Write-Host "Copying install.wim from source..." -ForegroundColor white -BackgroundColor blue
 
 # Download the latest Windows 11 ISO from the Microsoft Visual Studio Online portal. 
-# Mount the ISO and extract the install.wim file from the D:\Sources folder and place it in C:\ImageStaging:
-copy-item D:\sources\install.wim -destination C:\ImageStaging\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false 
+# Mount the ISO and extract the install.wim file from the D:\Sources folder and place it in R:\ImageStaging:
+copy-item D:\sources\install.wim -destination R:\ImageStaging\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false 
 
 ### Step 2 (Optional)
 # Check the index of the WIM file using the following command (if it's not the default of 5, make a note of it):
-# dism /Get-WimInfo /WimFile:C:\ImageStaging\install.wim
+# dism /Get-WimInfo /WimFile:R:\ImageStaging\install.wim
 
 ### Step 3 (Copy install.wim to device folders)
-copy-item C:\ImageStaging\install.wim -destination C:\ImageStaging\LenovoThinkBookG6\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false 
-copy-item C:\ImageStaging\install.wim -destination C:\ImageStaging\SurfacePro9\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false 
-copy-item C:\ImageStaging\install.wim -destination C:\ImageStaging\SurfaceGo4\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false 
-copy-item C:\ImageStaging\install.wim -destination C:\ImageStaging\ThinkCentreM70sG3\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false
-copy-item C:\ImageStaging\install.wim -destination C:\ImageStaging\Win11Pro_Generic\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false 
+copy-item R:\ImageStaging\install.wim -destination R:\ImageStaging\LenovoThinkBookG6\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false 
+copy-item R:\ImageStaging\install.wim -destination R:\ImageStaging\SurfacePro9\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false 
+copy-item R:\ImageStaging\install.wim -destination R:\ImageStaging\SurfaceGo4\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false 
+copy-item R:\ImageStaging\install.wim -destination R:\ImageStaging\ThinkCentreM70sG3\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false
+copy-item R:\ImageStaging\install.wim -destination R:\ImageStaging\Win11Pro_Generic\install.wim -PassThru | Set-ItemProperty -name IsReadOnly -Value $false 
 
 Write-Host "Install.wim copied successfully!" -ForegroundColor white -BackgroundColor darkgreen
 Write-Host ""
@@ -30,20 +36,20 @@ Write-Host ""
 Write-Host "Starting Windows 11 Pro Generic Image Update" -ForegroundColor white -BackgroundColor blue
 
 ## -- Mount Image -- ##
-Dism /Mount-Image /ImageFile:C:\ImageStaging\Win11Pro_Generic\Install.wim /MountDir:C:\ImageStaging\Win11Pro_Generic\Mount /Index:5
+Dism /Mount-Image /ImageFile:R:\ImageStaging\Win11Pro_Generic\Install.wim /MountDir:R:\ImageStaging\Win11Pro_Generic\Mount /Index:5
 
 ## -- Unmount WIM and Commit Changes -- ##
-Dism /Unmount-Image /MountDir:C:\ImageStaging\Win11Pro_Generic\Mount /Commit
+Dism /Unmount-Image /MountDir:R:\ImageStaging\Win11Pro_Generic\Mount /Commit
 
 ## -- Convert WIM to ESD -- ##
-Dism /Export-Image /SourceImageFile:C:\ImageStaging\Win11Pro_Generic\Install.wim /SourceIndex:5 /DestinationImageFile:C:\ImageStaging\Win11Pro_Generic\Win11Pro_Generic.esd /Compress:recovery /CheckIntegrity
+Dism /Export-Image /SourceImageFile:R:\ImageStaging\Win11Pro_Generic\Install.wim /SourceIndex:5 /DestinationImageFile:R:\ImageStaging\Win11Pro_Generic\Win11Pro_Generic.esd /Compress:recovery /CheckIntegrity
 
 ## -- Move ESD to IntePub -- ##
-Move-Item c:\ImageStaging\Win11Pro_Generic\Win11Pro_Generic.esd -Destination c:\inetpub\wwwroot\esd\Win11Pro_Generic.esd
+Move-Item R:\ImageStaging\Win11Pro_Generic\Win11Pro_Generic.esd -Destination c:\inetpub\wwwroot\esd\Win11Pro_Generic.esd
 Write-Host "Windows 11 Pro (Generic) Move Successful" -ForegroundColor white -BackgroundColor darkgreen
 
 ## -- Remove install.wim -- ##
-Remove-Item c:\ImageStaging\Win11Pro_Generic\install.wim
+Remove-Item R:\ImageStaging\Win11Pro_Generic\install.wim
 
 # Notify
 Write-Host "Windows 11 Pro (Generic) Update Complete" -ForegroundColor white -BackgroundColor darkgreen
@@ -56,23 +62,23 @@ Write-Host "Windows 11 Pro (Generic) Update Complete" -ForegroundColor white -Ba
 Write-Host "Starting Lenovo ThinkBook G6 Image Update" -ForegroundColor white -BackgroundColor blue
 
 ## -- Mount Image -- ##
-Dism /Mount-Image /ImageFile:C:\ImageStaging\LenovoThinkBookG6\install.wim /MountDir:C:\ImageStaging\LenovoThinkBookG6\Mount /Index:5
+Dism /Mount-Image /ImageFile:R:\ImageStaging\LenovoThinkBookG6\install.wim /MountDir:R:\ImageStaging\LenovoThinkBookG6\Mount /Index:5
 
 ## -- Add Drivers -- ##
-Dism /Image:C:\ImageStaging\LenovoThinkBookG6\Mount /Add-Driver /Driver:C:\Drivers\LenovoThinkBookG6 /Recurse
+Dism /Image:R:\ImageStaging\LenovoThinkBookG6\Mount /Add-Driver /Driver:C:\Drivers\LenovoThinkBookG6 /Recurse
 
 ## -- Unmount WIM and Commit Changes -- ##
-Dism /Unmount-Image /MountDir:C:\ImageStaging\LenovoThinkBookG6\Mount /Commit
+Dism /Unmount-Image /MountDir:R:\ImageStaging\LenovoThinkBookG6\Mount /Commit
 
 ## -- Convert WIM to ESD -- ##
-Dism /Export-Image /SourceImageFile:C:\ImageStaging\LenovoThinkBookG6\install.wim /SourceIndex:5 /DestinationImageFile:C:\ImageStaging\LenovoThinkBookG6\Win11_LenovoThinkBookG6.esd /Compress:recovery /CheckIntegrity
+Dism /Export-Image /SourceImageFile:R:\ImageStaging\LenovoThinkBookG6\install.wim /SourceIndex:5 /DestinationImageFile:R:\ImageStaging\LenovoThinkBookG6\Win11_LenovoThinkBookG6.esd /Compress:recovery /CheckIntegrity
 
 ## -- Move ESD to IntePub -- ##
-Move-Item c:\ImageStaging\LenovoG6\Win11_LenovoThinkBookG6.esd -Destination c:\inetpub\wwwroot\esd\Win11_LenovoThinkBookG6.esd
+Move-Item R:\ImageStaging\LenovoG6\Win11_LenovoThinkBookG6.esd -Destination c:\inetpub\wwwroot\esd\Win11_LenovoThinkBookG6.esd
 Write-Host "Lenovo ThinkBook G6 Move Successful" -ForegroundColor white -BackgroundColor darkgreen
 
 ## -- Remove install.wim -- ##
-Remove-Item c:\ImageStaging\LenovoG6\install.wim
+Remove-Item R:\ImageStaging\LenovoG6\install.wim
 
 # Notify
 Write-Host "Lenovo G6 Update Complete" -ForegroundColor white -BackgroundColor darkgreen
@@ -85,23 +91,23 @@ Write-Host "Lenovo G6 Update Complete" -ForegroundColor white -BackgroundColor d
 Write-Host "Starting Surface Pro 9 Image Update" -ForegroundColor white -BackgroundColor blue
 
 ## -- Mount Image -- ##
-Dism /Mount-Image /ImageFile:C:\ImageStaging\SurfacePro9\install.wim /MountDir:C:\ImageStaging\SurfacePro9\Mount /Index:5
+Dism /Mount-Image /ImageFile:R:\ImageStaging\SurfacePro9\install.wim /MountDir:R:\ImageStaging\SurfacePro9\Mount /Index:5
 
 ## -- Add Drivers -- ##
-Dism /Image:C:\ImageStaging\SurfacePro9\Mount /Add-Driver /Driver:C:\Drivers\SurfacePro9 /Recurse
+Dism /Image:R:\ImageStaging\SurfacePro9\Mount /Add-Driver /Driver:C:\Drivers\SurfacePro9 /Recurse
 
 ## -- Unmount WIM and Commit Changes -- ##
-Dism /Unmount-Image /MountDir:C:\ImageStaging\SurfacePro9\Mount /Commit
+Dism /Unmount-Image /MountDir:R:\ImageStaging\SurfacePro9\Mount /Commit
 
 ## -- Convert WIM to ESD -- ##
-Dism /Export-Image /SourceImageFile:C:\ImageStaging\SurfacePro9\install.wim /SourceIndex:5 /DestinationImageFile:C:\ImageStaging\SurfacePro9\Win11_SurfacePro9.esd /Compress:recovery /CheckIntegrity
+Dism /Export-Image /SourceImageFile:R:\ImageStaging\SurfacePro9\install.wim /SourceIndex:5 /DestinationImageFile:R:\ImageStaging\SurfacePro9\Win11_SurfacePro9.esd /Compress:recovery /CheckIntegrity
 
 ## -- Move ESD to IntePub -- ##
-Move-Item c:\ImageStaging\SurfacePro9\Win11_SurfacePro9.esd -Destination c:\inetpub\wwwroot\esd\Win11_SurfacePro9.esd
+Move-Item R:\ImageStaging\SurfacePro9\Win11_SurfacePro9.esd -Destination c:\inetpub\wwwroot\esd\Win11_SurfacePro9.esd
 Write-Host "Surface Pro 9 Move Successful" -ForegroundColor white -BackgroundColor darkgreen
 
 ## -- Remove install.wim -- ##
-Remove-Item c:\ImageStaging\SurfacePro9\install.wim
+Remove-Item R:\ImageStaging\SurfacePro9\install.wim
 
 # Notify
 Write-Host "Surface Pro 9 Update Complete" -ForegroundColor white -BackgroundColor darkgreen
@@ -114,23 +120,23 @@ Write-Host "Surface Pro 9 Update Complete" -ForegroundColor white -BackgroundCol
 Write-Host "Starting Surface Go 4 Image Update" -ForegroundColor white -BackgroundColor blue
 
 ## -- Mount Image -- ##
-Dism /Mount-Image /ImageFile:C:\ImageStaging\SurfaceGo4\install.wim /MountDir:C:\ImageStaging\SurfaceGo4\Mount /Index:5
+Dism /Mount-Image /ImageFile:R:\ImageStaging\SurfaceGo4\install.wim /MountDir:R:\ImageStaging\SurfaceGo4\Mount /Index:5
 
 ## -- Add Drivers -- ##
-Dism /Image:C:\ImageStaging\SurfaceGo4\Mount /Add-Driver /Driver:c:\Drivers\SurfaceGo4 /Recurse
+Dism /Image:R:\ImageStaging\SurfaceGo4\Mount /Add-Driver /Driver:c:\Drivers\SurfaceGo4 /Recurse
 
 ## -- Unmount WIM and Commit Changes -- ##
-Dism /Unmount-Image /MountDir:C:\ImageStaging\SurfaceGo4\Mount /Commit
+Dism /Unmount-Image /MountDir:R:\ImageStaging\SurfaceGo4\Mount /Commit
 
 ## -- Convert WIM to ESD -- ##
-Dism /Export-Image /SourceImageFile:C:\ImageStaging\SurfaceGo4\install.wim /SourceIndex:5 /DestinationImageFile:C:\ImageStaging\SurfaceGo4\install.esd /Compress:recovery /CheckIntegrity
+Dism /Export-Image /SourceImageFile:R:\ImageStaging\SurfaceGo4\install.wim /SourceIndex:5 /DestinationImageFile:R:\ImageStaging\SurfaceGo4\install.esd /Compress:recovery /CheckIntegrity
  
 ## -- Move ESD to IntePub -- ##
-Move-Item c:\ImageStaging\SurfaceGo4\Win11_SurfaceGo4.esd -Destination c:\inetpub\wwwroot\esd\Win11_SurfaceGo4.esd
+Move-Item R:\ImageStaging\SurfaceGo4\Win11_SurfaceGo4.esd -Destination c:\inetpub\wwwroot\esd\Win11_SurfaceGo4.esd
 Write-Host "Surface Go 4 Move Successful" -ForegroundColor white -BackgroundColor darkgreen
 
 ## -- Remove install.wim -- ##
-Remove-Item c:\ImageStaging\SurfaceGo4\install.wim
+Remove-Item R:\ImageStaging\SurfaceGo4\install.wim
 
 # Notify
 Write-Host "Surface Go 4 Update Complete" -ForegroundColor white -BackgroundColor darkgreen
@@ -143,23 +149,23 @@ Write-Host "Surface Go 4 Update Complete" -ForegroundColor white -BackgroundColo
 Write-Host "Starting ThinkCentre M70s G3 Update" -ForegroundColor white -BackgroundColor blue
 
 ## -- Mount Image -- ##
-Dism /Mount-Image /ImageFile:C:\ImageStaging\ThinkCentreM70sG3\Install.wim /MountDir:C:\ImageStaging\ThinkCentreM70sG3\Mount /Index:5
+Dism /Mount-Image /ImageFile:R:\ImageStaging\ThinkCentreM70sG3\Install.wim /MountDir:R:\ImageStaging\ThinkCentreM70sG3\Mount /Index:5
 
 ## -- Add Drivers -- ##
-Dism /Image:C:\ImageStaging\ThinkCentreM70sG3\Mount /Add-Driver /Driver:C:\Drivers\ThinkCentreM70sG3 /Recurse
+Dism /Image:R:\ImageStaging\ThinkCentreM70sG3\Mount /Add-Driver /Driver:C:\Drivers\ThinkCentreM70sG3 /Recurse
 
 ## -- Unmount WIM and Commit Changes -- ##
-Dism /Unmount-Image /MountDir:C:\ImageStaging\ThinkCentreM70sG3\Mount /Commit
+Dism /Unmount-Image /MountDir:R:\ImageStaging\ThinkCentreM70sG3\Mount /Commit
 
 ## -- Convert WIM to ESD -- ##
-Dism /Export-Image /SourceImageFile:C:\ImageStaging\ThinkCentreM70sG3\Install.wim /SourceIndex:5 /DestinationImageFile:C:\ImageStaging\ThinkCentreM70sG3\ThinkCentreM70sG3.esd /Compress:recovery /CheckIntegrity
+Dism /Export-Image /SourceImageFile:R:\ImageStaging\ThinkCentreM70sG3\Install.wim /SourceIndex:5 /DestinationImageFile:R:\ImageStaging\ThinkCentreM70sG3\ThinkCentreM70sG3.esd /Compress:recovery /CheckIntegrity
 
 ## -- Move ESD to IntePub -- ##
-Move-Item c:\ImageStaging\ThinkCentreM70sG3\ThinkCentreM70sG3.esd -Destination c:\inetpub\wwwroot\esd\ThinkCentreM70sG3.esd
+Move-Item R:\ImageStaging\ThinkCentreM70sG3\ThinkCentreM70sG3.esd -Destination c:\inetpub\wwwroot\esd\ThinkCentreM70sG3.esd
 Write-Host "Lenovo ThinkCentre M70s Move Successful" -ForegroundColor white -BackgroundColor darkgreen
 
 ## -- Remove install.wim -- ##
-Remove-Item c:\ImageStaging\ThinkCentreM70sG3\install.wim
+Remove-Item R:\ImageStaging\ThinkCentreM70sG3\install.wim
 
 # Notify
 Write-Host "ThinkCentre M70s Update Complete" -ForegroundColor white -BackgroundColor darkgreen
