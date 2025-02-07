d184d81b7b6cbd2e18fd42b4044cb145006f83da
DaveBrophy <40424676+DaveBrophy@users.noreply.github.com>
2025-02-07 12:04:57 +0000

    Update autopilotjoin.ps1

--- autopilotjoin.ps1
+++ autopilotjoin.ps1
@@ -1,6 +1,6 @@
 Set-executionpolicy bypass -scope process
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 #PowerShell.exe -ExecutionPolicy Bypass
-Install-Script -name Get-WindowsAutopilotInfo -Force
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Install-Script -name Get-WindowsAutopilotInfo -Force
 Get-WindowsAutopilotInfo -Online
